ad5a2d68a0e29c62129e649d36fe1b1ba97f0571
柏 <1184940148@qq.com>
2022-09-09 21:30:42 +0800

    代码优化

--- src/robot/pkg/solia/solitaireMth.go
+++ src/robot/pkg/solia/solitaireMth.go
@@ -42,7 +42,7 @@ type Solia struct {
 	UserId string //正在接龙的用户id
 	StrSet Set    //已经使用过的成语
 	flag   bool   //是否开始接龙
-	rd     *bufio.Reader
+	rd     []string
 	tryNum int    // 尝试次数
 	nowStr string //当前接龙的成语
 }
@@ -75,40 +75,25 @@ func (s *Solia) ReadStart(userID string) (string, error) {
 }
 
 func (s *Solia) readLineNum(lineNum int) (string, error) {
-	num := 1
-	strPath, _ := os.Getwd()
-	fmt.Println(strPath[:strings.LastIndex(strPath, "robot")+5])
-	strPath = strPath[:strings.LastIndex(strPath, "robot")+5]
-	file, err := os.Open(strPath + "./idiom.txt") //只是用来读的时候，用os.Open。绝对路径，获取robot路径
-	if err != nil {
-		fmt.Printf("打开文件失败,err:%v\n", err)
-		return "", err
-	}
-	defer file.Close()                   //关闭文件,为了避免文件泄露和忘记写关闭文件
-	decoder := mahonia.NewDecoder("gbk") //转码，避免中文字符乱码
-	//使用buffio读取文件内容
-	reader := bufio.NewReader(decoder.NewReader(file)) //创建新的读的对象
-	for {
-		line, err := reader.ReadString('\n') //注意是字符，换行符。
-		if err == io.EOF {
-			fmt.Println("文件读完了")
-			break
-		}
-		if err != nil { //错误处理
-			fmt.Printf("读取文件失败,错误为:%v", err)
+	//成语数据为空，则重新获取
+	if s.rd == nil {
+		err := s.getFiles()
+		if err != nil {
 			return "", err
 		}
-		num++
-		line = strings.Replace(line, "\r\n", "", -1)
-		if num >= lineNum && utf8.RuneCountInString(line) == 4 {
+	}
+	for i := 0; i < len(s.rd); i++ {
+		line := s.rd[i]
+		if i+1 >= lineNum && utf8.RuneCountInString(line) == 4 {
 			return line, nil
 		}
-		//fmt.Println(utf8.RuneCountInString(line),string(line))
 	}
 	return "人山人海", nil
 }
 
 func (s *Solia) ReadStr(content string) (string, error) {
+	strs := strings.Split(content, ">")
+	content = strings.TrimSpace(strs[len(strs)-1])
 	if s.StrSet.Contains(content) { //判断是否重复的成语
 		if s.tryNum > 1 {
 			s.tryNum--
@@ -118,8 +103,6 @@ func (s *Solia) ReadStr(content string) (string, error) {
 			return "", errors.New(fmt.Sprintf(ContainsOver3))
 		}
 	}
-	strs := strings.Split(content, "> ")
-	content = strs[len(strs)-1]
 	s1 := string([]rune(s.nowStr)[3:])
 	if s1 != string([]rune(content)[:1]) { //判断首字是否和上一个成语尾字一样
 		if s.tryNum > 1 {
@@ -135,30 +118,14 @@ func (s *Solia) ReadStr(content string) (string, error) {
 	if len([]rune(content)) >= 4 {
 		str1 := string([]rune(content)[3:])
 
-		strPath, _ := os.Getwd()
-		fmt.Println(strPath[:strings.LastIndex(strPath, "robot")+5])
-		strPath = strPath[:strings.LastIndex(strPath, "robot")+5]
-		file, err := os.Open(strPath + "./idiom.txt") //只是用来读的时候，用os.Open。绝对路径，获取robot路径
-		if err != nil {
-			fmt.Printf("打开文件失败,err:%v\n", err)
-			return "", err
+		if s.rd == nil {
+			err := s.getFiles()
+			if err != nil {
+				return "", err
+			}
 		}
-		defer file.Close()                   //关闭文件,为了避免文件泄露和忘记写关闭文件
-		decoder := mahonia.NewDecoder("gbk") //转码，避免中文字符乱码
-		//使用buffio读取文件内容
-		reader := bufio.NewReader(decoder.NewReader(file)) //创建新的读的对象
-		for {
-			line, err := reader.ReadString('\n') //注意是字符，换行符。
-			if err == io.EOF {
-				fmt.Println("文件读完了")
-				break
-			}
-			if err != nil { //错误处理
-				fmt.Printf("读取文件失败,错误为:%v", err)
-				s.ReNew()
-				return "", errors.New("我不会了，你赢了")
-			}
-			line = strings.Replace(line, "\r\n", "", -1)
+		for i := 0; i < len(s.rd); i++ {
+			line := s.rd[i]
 			if str1 == string([]rune(line)[:1]) && !s.StrSet.Contains(line) {
 				res = line
 			}
@@ -168,7 +135,6 @@ func (s *Solia) ReadStr(content string) (string, error) {
 			if flag && res != "" {
 				break
 			}
-			//fmt.Println(utf8.RuneCountInString(line),string(line))
 		}
 	}
 	if !flag {
@@ -193,4 +159,34 @@ func (s *Solia) ReadStr(content string) (string, error) {
 		s.nowStr = res
 	}
 	return res, nil
+}
+
+func (s *Solia) getFiles() error {
+	strPath, _ := os.Getwd()
+	strPath = strPath[:strings.LastIndex(strPath, "robot")+5]
+	file, err := os.Open(strPath + "./idiom.txt") //只是用来读的时候，用os.Open。绝对路径，获取robot路径
+	if err != nil {
+		fmt.Printf("打开文件失败,err:%v\n", err)
+		return err
+	}
+	defer file.Close()                                 //关闭文件,为了避免文件泄露和忘记写关闭文件
+	decoder := mahonia.NewDecoder("gbk")               //转码，避免中文字符乱码
+	reader := bufio.NewReader(decoder.NewReader(file)) //创建新的读的对象
+	var chunks []string
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.Replace(line, "\r\n", "", -1)
+		if err == io.EOF {
+			fmt.Println("文件读完了")
+			break
+		}
+		if err != nil { //错误处理
+			fmt.Printf("读取文件失败,错误为:%v", err)
+			return err
+		}
+
+		chunks = append(chunks, line)
+	}
+	s.rd = chunks
+	return nil
 }
--- src/robot/pkg/solia/solitaireMth_test.go
+++ src/robot/pkg/solia/solitaireMth_test.go
@@ -1,7 +1,6 @@
 package solia
 
 import (
-	"bufio"
 	"testing"
 )
 
@@ -10,7 +9,7 @@ func TestSolia_ReadStart(t *testing.T) {
 		UserId string
 		StrSet Set
 		flag   bool
-		rd     *bufio.Reader
+		rd     []string
 		tryNum int
 		nowStr string
 	}
@@ -25,7 +24,7 @@ func TestSolia_ReadStart(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "test1",
+			name: "test1(正常开始)", //正常开始，成语接龙
 			fields: fields{
 				UserId: "",
 				StrSet: Set{
@@ -36,8 +35,47 @@ func TestSolia_ReadStart(t *testing.T) {
 				tryNum: 3,
 				nowStr: "",
 			},
+			args: args{
+				userID: "1313",
+			},
 			want:    "人山人海",
 			wantErr: false,
+		},
+		{
+			name: "test2", //已经开始成语接龙，其他人再次调用
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "",
+			},
+			args: args{
+				userID: "1313",
+			},
+			want:    "人山人海",
+			wantErr: true,
+		},
+		{
+			name: "test3(重复开始)", //已经开始成语接龙，本人再次调用
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "",
+			},
+			args: args{
+				userID: "13131",
+			},
+			want:    "人山人海",
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -55,8 +93,200 @@ func TestSolia_ReadStart(t *testing.T) {
 				t.Errorf("ReadStart() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != "" && len(got) == len(tt.want) {
+			if got != "" && len(got) != len(tt.want) {
 				t.Errorf("ReadStart() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolia_readLineNum(t *testing.T) {
+	type fields struct {
+		UserId string
+		StrSet Set
+		flag   bool
+		rd     []string
+		tryNum int
+		nowStr string
+	}
+	type args struct {
+		lineNum int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test1", //获取1000行后，第一个四字成语
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "",
+			},
+			args: args{
+				lineNum: 1000,
+			},
+			want:    "不足为训",
+			wantErr: false,
+		},
+		{
+			name: "test2", //获取-1行后，第一个四字成语
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "",
+			},
+			args: args{
+				lineNum: -1,
+			},
+			want:    "阿鼻地狱",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Solia{
+				UserId: tt.fields.UserId,
+				StrSet: tt.fields.StrSet,
+				flag:   tt.fields.flag,
+				rd:     tt.fields.rd,
+				tryNum: tt.fields.tryNum,
+				nowStr: tt.fields.nowStr,
+			}
+			got, err := s.readLineNum(tt.args.lineNum)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("readLineNum() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("readLineNum() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolia_ReadStr(t *testing.T) {
+	type fields struct {
+		UserId string
+		StrSet Set
+		flag   bool
+		rd     []string
+		tryNum int
+		nowStr string
+	}
+	type args struct {
+		content string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "test1", //接不出下一个成语
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "声威大震",
+			},
+			args: args{
+				content: "<@21321321> 震耳欲聋",
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "test2", //输入非成语
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "声威大震",
+			},
+			args: args{
+				content: "<@21321321> 乱写的",
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "test3", //正常接出下一个成语
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "声威大震",
+			},
+			args: args{
+				content: "<@21321321> 震天动地",
+			},
+			want:    "地主之谊",
+			wantErr: false,
+		},
+		{
+			name: "test4(首字和上一个成语不一样)", //首字和上一个成语不一样
+			fields: fields{
+				UserId: "13131",
+				StrSet: Set{
+					m: make(map[interface{}]struct{}),
+				},
+				flag:   false,
+				rd:     nil,
+				tryNum: 3,
+				nowStr: "声威大震",
+			},
+			args: args{
+				content: "<@21321321> 天崩地裂",
+			},
+			want:    "",
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Solia{
+				UserId: tt.fields.UserId,
+				StrSet: tt.fields.StrSet,
+				flag:   tt.fields.flag,
+				rd:     tt.fields.rd,
+				tryNum: tt.fields.tryNum,
+				nowStr: tt.fields.nowStr,
+			}
+			got, err := s.ReadStr(tt.args.content)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadStr() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("ReadStr() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
